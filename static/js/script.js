b5b275c657464df467f6aa87b8c97a597ba176b3
YonahPy <jrjonas487@gmail.com>
2023-10-17 12:38:25 -0300

    navegação minimizada

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,31 @@
 const add = document.querySelector('#add');
 const exclude = document.querySelector('#delete');
 const edit = document.querySelector('#edit');
-const links = document.querySelector('.links')
-const input = document.querySelectorAll('.links input')
-const select = document.querySelector('select')
-const nomeSite = document.querySelector('.nome-site')
+const links = document.querySelector('.links');
+const input = document.querySelectorAll('.links input');
+const select = document.querySelector('select');
+const nomeSite = document.querySelector('.nome-site');
+const categorias = document.querySelectorAll('.titulo');
 
 
 add.addEventListener('click', () => {
     select.classList.toggle('active')
     input.forEach(i => {
         i.classList.toggle('active')
-        i.textContent = ''
+        i.value = ''
     }); 
     
 })
 
-nomeSite.addEventListener('click', () => {
-    
+categorias.forEach((categoria) => {
+    categoria.addEventListener('click', () => {
+        document.querySelectorAll('.name').forEach((link) => {
+            link.style.display = 'none';
+        });
+
+        const categoriaId = categoria.id;
+        document.querySelectorAll(`.name[data-categoria="${categoriaId}"]`).forEach((link) => {
+            link.style.display = 'flex';
+        })
+    })
 })
